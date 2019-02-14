c6e7b71e50597e41ca1db52f5656f372c5908662
Vladimir V. Myagdeev <vvmspace@gmail.com>
2019-02-14 18:45:22 +0500

    not empty() first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * @param {Mixed} val
+ * @return {Boolean}
+ */
+
+const empty = value => {
+  
+  // If value is null on undefined:
+  if (value == null) return true;
+
+  // By types:
+  if ('boolean' == typeof value) return false;
+  if ('number' == typeof value) return value === 0;
+  if ('string' == typeof value) return value.length === 0;
+  if ('function' == typeof value) return value.length === 0;
+  
+  // Empty arrays:
+  if (Array.isArray(value)) return value.length === 0;
+  
+  // Error with empty message:
+  if (value instanceof Error) return value.message === '';
+  
+  // Empty objects:
+  if (value.toString == Object.prototype.toString) {
+    switch (value.toString()) {
+
+      case '[object File]':
+      case '[object Map]':
+      case '[object Set]': {
+        return value.size === 0
+      }
+      case '[object Object]': {
+        for (let key in value) {
+          if (Object.prototype.hasOwnProperty.call(value, key)) return false
+        }
+
+        return true
+      }
+    }
+  }
+
+  // Everything else...
+  return false
+}
+
+/**
+ * @type {Function}
+ */
+
+module.exports = empty;
---
+++ test/index.js
@@ -0,0 +1,60 @@
+var assert = require('assert')
+var empty = require('../index.js')
+
+describe('empty-js', function () {
+
+  it('Testing nulls', function () {
+    assert(empty(null) == true)
+    assert(empty(undefined) == true)
+    assert(empty() == true)
+  })
+
+  it('Testing booleans', function () {
+    assert(empty(false) == false)
+    assert(empty(true) == false)
+  })
+
+  it('Testing arrays', function () {
+    assert(empty([]) == true)
+    assert(empty(['a', 'b']) == false)
+  })
+
+  it('Testing maps', function () {
+    assert(empty(new Map()) == true)
+    assert(empty(new Map([['key', 'value']])) == false)
+  })
+
+  it('Testing sets', function () {
+    assert(empty(new Set()) == true)
+    assert(empty(new Set([1,2,3,4])) == false)
+  })
+
+  it('Testing errors', function () {
+    assert(empty(new Error()) == true)
+    assert(empty(new Error('')) == true)
+    assert(empty(new Error('test')) == false)
+  })
+
+  it('Testing objects', function () {
+    assert(empty({}) == true)
+    assert(empty({ a: 'b' }) == false)
+    assert(empty({ length: 0 }) == false)
+  })
+
+  it('Testing strings', function () {
+    assert(empty('') == true)
+    assert(empty('string') == false)
+    assert(empty('Error') == false)
+  })
+
+  it('Testing numbers', function () {
+    assert(empty(0) == true)
+    assert(empty(42) == false)
+  })
+
+  it('Testing functions', function () {
+    assert(empty(function(){}) == true)
+    assert(empty(function(a,b){}) == false)
+  })
+
+})
