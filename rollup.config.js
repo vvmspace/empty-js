46d6712236c330755e81173c08a54c856ab6ceb5
Niklas Ekman <niklas@stockfiller.com>
2020-05-16 13:38:11 +0200

    add usage to readme. minor rollup changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,20 @@
 import path from "path";
 import babel from "@rollup/plugin-babel";
+import pkg from "./package.json";
 
 const formats = [
     "esm",
     "cjs"
 ];
 
+const defaultFormat = {
+    exports: "default",
+    name: pkg.name,
+};
+
 // See: https://rollupjs.org/guide/en/#configuration-files
 export default {
     input: path.resolve(__dirname, "src/index.js"),
-    output: formats.map(format => ({ format, file: `dist/index.${format}.js` })),
+    output: formats.map(format => ({ ...defaultFormat, format, file: `dist/index.${format}.js` })),
     plugins: [babel({ babelHelpers: "bundled" })]
 }
