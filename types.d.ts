fb38875f689746ddfa00521e680926c69fa5ccd7
Niklas Ekman <nikl.ekman@gmail.com>
2020-12-16 17:20:40 +0100

    add an empty type

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,13 @@
 /**
+ * The definition of an empty value in PHP
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+export type Empty = undefined | null | "" | 0 | 0.0 | "0" | false | [];
+
+/**
  * Determines if the input value is empty
  * @param value The value to determine if empty
  * @returns true if the value is empty, false otherwise
  * @see https://www.php.net/manual/en/function.empty.php
  */
-export default function empty<T = unknown>(value: T | undefined | null): boolean;
+export default function empty(value: unknown): value is Empty;
